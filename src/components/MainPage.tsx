20d9e9d6457939d2e3584d841c87cedb5a46bad6
Artyom Ganev <a.a.ganev@gmail.com>
2019-01-13 16:01:28 +0300

    init project pt.2

---
+++ src/components/MainPage.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import './MainPage.css';
+
+/**
+ * Main page application
+ */
+class MainPage extends React.Component {
+    public render() {
+        return <h1>Hello!!</h1>;
+    }
+}
+
+export default MainPage;
