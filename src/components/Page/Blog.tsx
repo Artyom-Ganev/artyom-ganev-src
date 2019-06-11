672b495815d55dbae572f0b20bce252fe53cbd1e
Artyom Ganev <a.a.ganev@gmail.com>
2019-06-11 12:21:54 +0300

    2019-06-11: add new blog record + update dependencies

--- src/components/Page/Blog.tsx
+++ src/components/Page/Blog.tsx
@@ -13,9 +13,10 @@ const items = blog.data.sort((o1, o2) => {
     return date1 < date2 ? 1 : -1;
 }).map((item) => {
     return (
-        <div key={item.key}>
+        <div key={item.key} className="page-blog__itemContainer">
             <div className="page-blog__title">{item.date}</div>
             <div className="page-blog__item">{item.value}</div>
+            <a href={item.link} target="_blank" rel="noopener noreferrer">{item.linkCaption}</a>
         </div>
     );
 });
