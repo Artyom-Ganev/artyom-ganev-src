15ae6dfd037f378e09e4c4cad52527f987d3972c
Artem Ganev <a.a.ganev@gmail.com>
2019-10-13 21:54:56 +0300

    Add build for Github Pages

---
+++ build/precache-manifest.ac01d3d0c1abbd50032a9c0fdb9ee1ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7580302f689171d3fb23c3d8a7608ff9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72167ea5113982e355c4",
+    "url": "/static/css/main.fb25a4b0.chunk.css"
+  },
+  {
+    "revision": "f06cc11bcfd499324928",
+    "url": "/static/js/2.d7ded7e4.chunk.js"
+  },
+  {
+    "revision": "72167ea5113982e355c4",
+    "url": "/static/js/main.1213a298.chunk.js"
+  },
+  {
+    "revision": "48962c9feffc971b27d4",
+    "url": "/static/js/runtime-main.876ff4bd.js"
+  },
+  {
+    "revision": "bbb441b28eda7231ca7dfbf56787a393",
+    "url": "/static/media/artyom.bbb441b2.jpg"
+  }
+]);
