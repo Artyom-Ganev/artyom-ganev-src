8cbdb15220327024951160ce5543c3219dbb00d0
aa.ganev <aa.ganev@tensor.ru>
2019-11-27 14:42:12 +0300

    fix build

---
+++ precache-manifest.058da7ab72924cd85547debac9ca1c2f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0e73e2d46d85b98acdd5981b129ddfb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55dce3c2aac7382072a8",
+    "url": "/static/css/main.f957c119.chunk.css"
+  },
+  {
+    "revision": "8aeee85552adfcf2a5ce",
+    "url": "/static/js/2.32788693.chunk.js"
+  },
+  {
+    "revision": "55dce3c2aac7382072a8",
+    "url": "/static/js/main.79728dc6.chunk.js"
+  },
+  {
+    "revision": "130bbc63ff58aa8592e7",
+    "url": "/static/js/runtime-main.c55ac303.js"
+  },
+  {
+    "revision": "bbb441b28eda7231ca7dfbf56787a393",
+    "url": "/static/media/artyom.bbb441b2.jpg"
+  },
+  {
+    "revision": "47f71852355abed89f88524b5e79cc86",
+    "url": "/static/media/rybinsk.47f71852.jpg"
+  }
+]);
